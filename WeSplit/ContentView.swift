b41f2582b1908d0f616851a7f298c7d6f09aac4c
Marcelo Hammes <marcelo.hammes@arctouch.com>
2019-10-25 19:37:58 -0300

    Initial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Marcelo Hammes on 25/10/19.
+//  Copyright © 2019 Marcelo Hammes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
